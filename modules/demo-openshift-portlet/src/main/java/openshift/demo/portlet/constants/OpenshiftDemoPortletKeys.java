a1a8b24b51308083a453e51255061274ad9ca3e9
Ivan Volkov <ivan.volkov@aimprosoft.com>
2024-04-19 20:15:49 +0300

    first commit

---
+++ modules/demo-openshift-portlet/src/main/java/openshift/demo/portlet/constants/OpenshiftDemoPortletKeys.java
@@ -0,0 +1,11 @@
+package openshift.demo.portlet.constants;
+
+/**
+ * @author user
+ */
+public class OpenshiftDemoPortletKeys {
+
+	public static final String OPENSHIFTDEMO =
+		"openshift_demo_portlet_OpenshiftDemoPortlet";
+
+}
---
+++ modules/demo-openshift-portlet/src/main/java/openshift/demo/portlet/portlet/OpenshiftDemoPortlet.java
@@ -0,0 +1,30 @@
+package openshift.demo.portlet.portlet;
+
+import openshift.demo.portlet.constants.OpenshiftDemoPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author user
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=OpenshiftDemo",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + OpenshiftDemoPortletKeys.OPENSHIFTDEMO,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class OpenshiftDemoPortlet extends MVCPortlet {
+}
